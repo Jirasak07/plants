5904af3129198dd46896c72f362ebced1b6427ee
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2023-08-10 22:25:04 +0700

    v

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/' >
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
