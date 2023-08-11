16f97ceb447fbcc46ba083d15b45a2a5b3139d61
Jirasak07 <bee.110243@gmail.com>
2023-08-11 14:16:18 +0700

    xc

--- src/Pages/Setting.js
+++ src/Pages/Setting.js
@@ -1,6 +1,14 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function Setting() {
+  useEffect(()=>{
+    axios.post("https://rspg-kpppao.com/backend/Plant/SelectTambon",{
+      amphur:'714'
+    }).then((res)=>{
+      console.log(res.data)
+    })
+  },[])
   return (
     <div className="container-md">
       <div className="bg-white">setting</div>
